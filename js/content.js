92cc2353bba6a2a5f71d7fb58870dada83687c79
Matt Mecham <math.u@hotmail.com>
2017-09-27 17:50:49 -0600

    just before going to Jensen's

--- js/content.js
+++
@@ -1,8 +0,0 @@
-
-var firstImage = "carpet-1.jpg";
-
-var lastImage = ""
-
-var half = "Our warehouse has an enormous selection of in-stock carpet, tile, laminate, hardwood, and vinyl flooring. Choose from our huge in-stock selection or special order a different color from our extensive catalog. At Calvin Carpets we don’t just sell affordable carpet, we sell high quality carpet at discount prices. You'll find our carpet to be at the lowest prices in the state.";
-
-var four = "Calvin Carpets has been in business since 1976. As the oldest flooring store in Middleton, we are dedicated to provide you with experienced service.";
